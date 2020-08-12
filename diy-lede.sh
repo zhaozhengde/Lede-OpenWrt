33b5ab615c1236c8d9ba73700703ea492c60ce2e
wx611975 <57552340+wx611975@users.noreply.github.com>
2020-08-12 14:52:11 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -8,7 +8,7 @@ rm -rf package/lean/luci-theme-argon
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
