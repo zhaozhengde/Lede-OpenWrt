e473c3bf1aa37ffd4e2dbee522c4dfb387b422af
wx611975 <57552340+wx611975@users.noreply.github.com>
2020-09-24 14:23:42 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -21,7 +21,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowS
 
 # Add a feed source增加默认源地址
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git' feeds.conf.default
+sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci' feeds.conf.default
 
 
 
