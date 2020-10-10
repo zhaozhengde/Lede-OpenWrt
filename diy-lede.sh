da78c5f346db63b22425a6ac3cc12946a735362b
wx611975 <57552340+wx611975@users.noreply.github.com>
2020-10-10 10:06:46 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -19,6 +19,17 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 
+source ../version
+sed -i '92d'                                                                   package/system/opkg/Makefile
+sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
+sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
+sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
+sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"  package/lean/default-settings/files/zzz-default-settings
+sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
+
 # Add a feed source增加默认源地址
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
