554287fcd519034674e7b6726fd361da46b6f81d
zhaozhengde <89142983+zhaozhengde@users.noreply.github.com>
2022-01-09 20:29:04 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -8,10 +8,10 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.123.1修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把XiaomiR3G修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/XiaomiR3G/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/RASPI/g' ./package/base-files/files/bin/config_generate
 
 
 
